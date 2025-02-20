4c11a7cad3ed4e2f69058e28b054054af32a15c5
VBrawl <konstantosjim@gmail.com>
2025-02-20 10:50:34 +0200

    Added more utils

--- builder/30_utils.sh
+++ builder/30_utils.sh
@@ -13,13 +13,13 @@ if [ ! -f busybox ]
 then
   make defconfig
   echo CONFIG_STATIC=y >> $BB_SRC/.config
-  make
+  make -j$(nproc)
 fi
 
 cp $BB_SRC/busybox $BUILD_BIN/busybox
 cd $BUILD
 
-for s in ls cat
+for s in ls cat pwd mount umount mkdir touch rm
 do
   ln -s /usr/bin/busybox usr/bin/$s
 done
