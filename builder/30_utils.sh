d6f7afb020fdffcd029a0c7d7c9ef67f01438256
VBrawl <konstantosjim@gmail.com>
2025-02-20 02:14:41 +0200

    Added busybox utils support (Only ls and cat for now)

---
+++ builder/30_utils.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BB_SRC=$CACHE/busybox
+
+if [ ! -d $BB_SRC ]
+then
+  git clone git://git.busybox.net/busybox $BB_SRC --depth 1
+fi
+
+cd $BB_SRC
+
+if [ ! -f busybox ]
+then
+  make defconfig
+  echo CONFIG_STATIC=y >> $BB_SRC/.config
+  make
+fi
+
+cp $BB_SRC/busybox $BUILD_BIN/busybox
+cd $BUILD
+
+for s in ls cat
+do
+  ln -s /usr/bin/busybox usr/bin/$s
+done
