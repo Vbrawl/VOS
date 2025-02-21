a50bd94551a6ab5a99e0f147b29e509c9b53265d
VBrawl <konstantosjim@gmail.com>
2025-02-22 01:17:40 +0200

    Added partitioning (1 whole partition) and filesystem creation (ext4)

--- builder/30_utils.sh
+++ builder/30_utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BB_SRC=$CACHE/busybox
+E2FSPROGS_SRC=$CACHE/e2fsprogs
 
 if [ ! -d $BB_SRC ]
 then
@@ -19,7 +20,20 @@ fi
 cp $BB_SRC/busybox $BUILD_BIN/busybox
 cd $BUILD
 
-#for s in ls cat pwd mount umount mkdir touch rm
-#do
-#  ln -s /usr/bin/busybox usr/bin/$s
-#done
+
+
+
+if [ ! -d $E2FSPROGS_SRC ]
+then
+  git clone git://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git $E2FSPROGS_SRC --depth 1
+fi
+
+cd $E2FSPROGS_SRC
+
+if [ ! -f misc/mke2fs ]
+then
+  ./configure --disable-nls CFLAGS="--static" LDFLAGS="--static"
+  make -j$(nproc)
+fi
+
+cp $E2FSPROGS_SRC/misc/mke2fs $BUILD_BIN/mke2fs
--- fs/installer/install.sh
+++ fs/installer/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/busybox ash
 
-read -p "Welcome to VOS Installer, would you like to proceed with the installation? [y/N] " PROCEED
+read -p "Welcome to VOS, would you like to proceed with the installation? [y/N] " PROCEED
 PROCEED=$(echo "${PROCEED:0:1}" | tr '[:lower:]' '[:upper:]')
 
 if [ "${PROCEED:0:1}" != "Y" ]
@@ -14,5 +14,33 @@ then
   exit
 fi
 
-echo "Not Quitting"
-ash
+fdisk -l | grep Disk
+read -p "Where should we install VOS? " DRIVE
+
+while ! fdisk -l $DRIVE 2>/dev/null
+do
+  echo "Invalid input!"
+  fdisk -l | grep Disk
+  read -p "Where should we install VOS? " DRIVE
+done
+
+fdisk $DRIVE << EOF
+o
+n
+p
+1
+
+
+a
+1
+w
+q
+EOF
+
+/usr/bin/mke2fs -t ext4 "${DRIVE}1"
+
+
+mkdir -p /mnt
+mount "${DRIVE}1" /mnt
+
+chvt 1
