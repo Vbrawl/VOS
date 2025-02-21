5f41674082ecce0062a4471f56f63c231dc15675
VBrawl <konstantosjim@gmail.com>
2025-02-21 22:32:22 +0200

    Added ttys and starting install script at vt7

--- builder/70_init_script.sh
+++ builder/70_init_script.sh
@@ -15,7 +15,15 @@ mount -t devtmpfs none /dev
 mount -t proc none /proc
 mount -t sysfs none /sys
 
-ash installer/install.sh
+
+openvt -c 2 -- getty -n -l /bin/ash 38400 tty2
+openvt -c 3 -- getty -n -l /bin/ash 38400 tty3
+openvt -c 4 -- getty -n -l /bin/ash 38400 tty4
+openvt -c 5 -- getty -n -l /bin/ash 38400 tty5
+openvt -c 6 -- getty -n -l /bin/ash 38400 tty6
+openvt -c 7 -- /bin/ash installer/install.sh
+
+getty -n -l /bin/ash 38400 tty1
 exec poweroff -f
 EOF
 
