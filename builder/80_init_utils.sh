1654ed58af62c6b1ffabc742b9c002ae3ad39943
VBrawl <konstantosjim@gmail.com>
2025-02-22 23:35:12 +0200

    Reorganized builder scripts

---
+++ builder/80_init_utils.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+BB_SRC=$CACHE/busybox
+E2FSPROGS_SRC=$CACHE/e2fsprogs
+
+if [ ! -d $BB_SRC ]
+then
+  git clone git://git.busybox.net/busybox $BB_SRC --depth 1
+fi
+
+cd $BB_SRC
+
+if [ ! -f busybox ]
+then
+  cp $CONFIGS/busybox.conf $BB_SRC/.config
+  make -j$(nproc)
+fi
+
+mkdir -p $BUILD_INITRD/bin
+cp $BB_SRC/busybox $BUILD_INITRD/bin/busybox
+
+
+
+
+if [ ! -d $E2FSPROGS_SRC ]
+then
+  git clone git://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git $E2FSPROGS_SRC --depth 1
+fi
+
+cd $E2FSPROGS_SRC
+
+if [ ! -f misc/mke2fs ]
+then
+  ./configure --disable-nls CFLAGS="--static" LDFLAGS="--static"
+  make -j$(nproc)
+fi
+
+cp $E2FSPROGS_SRC/misc/mke2fs $BUILD_INITRD/bin/mke2fs
