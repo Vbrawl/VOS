5bbf27cc1138ebf8e06abe542b57219e18d0ae77
VBrawl <konstantosjim@gmail.com>
2025-03-03 01:47:35 +0200

    Added openresolv

--- builder/70_networking.sh
+++ builder/70_networking.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
 DHCPCD_SRC=$CACHE/dhcpcd
+OPENRESOLV_SRC=$CACHE/openresolv
 
 if [ ! -d $DHCPCD_SRC ]
 then
-  wget https://github.com/NetworkConfiguration/dhcpcd/archive/refs/tags/v10.2.2.tar.gz
-  tar -xf v10.2.2.tar.gz
-  mv v10.2.2 $DHCPCD_SRC
+  cd $CACHE
+  wget https://github.com/NetworkConfiguration/dhcpcd/archive/refs/tags/v10.2.2.tar.gz -O dhcpcd-10.2.2.tar.gz
+  tar -xf dhcpcd-10.2.2.tar.gz
+  mv dhcpcd-10.2.2 $DHCPCD_SRC
 fi
 
 cd $DHCPCD_SRC
 
 if [ ! -f $DHCPCD_SRC/src/dhcpcd ]
 then
-  ../configure --prefix=/usr --target=$TARGET --host=$TARGET
+  ./configure --prefix=/usr --target=$TARGET --host=$TARGET
+  make -j$(nproc)
+fi
+
+make DESTDIR=$ISO_SYSROOT install
+
+
+if [ ! -d $OPENRESOLV_SRC ]
+then
+  cd $CACHE
+  wget https://github.com/NetworkConfiguration/openresolv/archive/refs/tags/v3.13.2.tar.gz -O openresolv-3.13.2.tar.gz
+  tar -xf openresolv-3.13.2.tar.gz
+  mv openresolv-3.13.2 $OPENRESOLV_SRC
+fi
+
+cd $OPENRESOLV_SRC
+
+if [ ! -f $OPENRESOLV_SRC/resolvconf ]
+then
+  ./configure --prefix=/usr --target=$TARGET --host=$TARGET
   make -j$(nproc)
 fi
 
