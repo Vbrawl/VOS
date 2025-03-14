c4f509eb1cffded18aff96e7850e00cd15e7974a
VBrawl <konstantosjim@gmail.com>
2025-03-14 16:54:10 +0200

    Fixed inetutils script

--- builder/utils/inetutils.sh
+++ builder/utils/inetutils.sh
@@ -2,19 +2,13 @@
 
 INETUTILS_SRC=$CACHE/inetutils
 
-download_and_untar "https://ftp.gnu.org/gnu/inetutils/inetutils-v2.6-src.tar.gz" "$INETUTILS_SRC"
-
-cd $INETUTILS_SRC
-./bootstrap
+download_and_untar "https://ftp.gnu.org/gnu/inetutils/inetutils-2.6.tar.xz" "$INETUTILS_SRC"
 
 if [ ! -d $INETUTILS_SRC/build ]
 then
-  cd $INETUTILS_SRC
-  ./bootstrap
-
   mkdir -p $INETUTILS_SRC/build
   cd $INETUTILS_SRC/build
-  ../configure --prefix=/usr --host=$TARGET --build=$(../build-aux/config.guess)
+  ../configure --prefix=/usr --host=$TARGET --build=$(../build-aux/config.guess) --enable-year2038 --disable-ifconfig
   make -j$(nproc)
 fi
 cd $INETUTILS_SRC/build
