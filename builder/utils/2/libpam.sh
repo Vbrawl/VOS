3489c37bb94baef066963155057c870aa9338439
VBrawl <konstantosjim@gmail.com>
2025-03-27 00:21:03 +0200

    Extracted libpam's patch to a patch file

--- builder/utils/2/libpam.sh
+++ builder/utils/2/libpam.sh
@@ -6,9 +6,7 @@ $ROOT/download_and_untar.sh "https://github.com/linux-pam/linux-pam/releases/dow
 if [ ! -d $PAM_SRC/build ]
 then
   cd $PAM_SRC
-  ## PATCH meson.build file to allow compilation
-  #  HAVE_CRYPT_H doesn't exist but it is used causing meson to fail
-  sed -i "s/cdata.get([\"|']HAVE_CRYPT_H[\"|'])/1/g" $PAM_SRC/meson.build
+  patch -p1 < $ROOT/patches/pam.patch
   meson setup build --cross-file $MESON_CROSS_FILE -Dc_args="-Wno-implicit-function-declaration -Wno-int-conversion -I${ISO_SYSROOT}/usr/include"
   cd $PAM_SRC/build
   meson compile
