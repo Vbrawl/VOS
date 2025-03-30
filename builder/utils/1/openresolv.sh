100114670097b25453ca1b7eafaeb799674b79ed
VBrawl <konstantosjim@gmail.com>
2025-03-31 02:03:23 +0300

    Added openresolv and patch as RPM packages

--- builder/utils/1/openresolv.sh
+++ builder/utils/1/openresolv.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 OPENRESOLV_SRC=$CACHE/openresolv
+OPENRESOLV_RPM_NAME=OpenResolv
+OPENRESOLV_VERSION="3.13.2"
+OPENRESOLV_FULLNAME=$OPENRESOLV_RPM_NAME-$OPENRESOLV_VERSION
 $ROOT/download_and_untar.sh "https://github.com/NetworkConfiguration/openresolv/archive/refs/tags/v3.13.2.tar.gz" "$OPENRESOLV_SRC"
 cd $OPENRESOLV_SRC
 
@@ -10,4 +13,28 @@ then
   make -j$(nproc)
 fi
 
-make DESTDIR=$ISO_SYSROOT install
+#make DESTDIR=$ISO_SYSROOT install
+$ROOT/generate_tar.sh make $OPENRESOLV_SRC/$OPENRESOLV_FULLNAME.tar $OPENRESOLV_FULLNAME \
+                      etc/resolvconf.conf \
+                      usr/lib/resolvconf/{dnsmasq,libc,named,pdnsd,pdns_recursor,unbound} \
+                      usr/lib/resolvconf/libc.d/{avahi-daemon,mdnsd} \
+                      usr/sbin/resolvconf
+
+$ROOT/generate_rpm.sh "$ISO_RPMS" \
+                      "$OPENRESOLV_SRC/rpmbuild" \
+                      "$OPENRESOLV_RPM_NAME" \
+                      "$OPENRESOLV_VERSION" \
+                      "OpenResolv utilities/libraries" \
+                      "openresolv is a resolvconf implementation which manages /etc/resolv.conf." \
+                      "BSD-2-Clause" \
+                      "$OPENRESOLV_SRC/$OPENRESOLV_FULLNAME.tar" \
+                      "etc/resolvconf.conf" "%{_sysconfdir}/resolvconf.conf" \
+                      "usr/lib/resolvconf/libc" "%{_libdir}/resolvconf/libc" \
+                      "usr/lib/resolvconf/dnsmasq" "%{_libdir}/resolvconf/dnsmasq" \
+                      "usr/lib/resolvconf/pdnsd" "%{_libdir}/resolvconf/pdnsd" \
+                      "usr/lib/resolvconf/unbound" "%{_libdir}/resolvconf/unbound" \
+                      "usr/lib/resolvconf/libc.d/avahi-daemon" "%{_libdir}/resolvconf/libc.d/avahi-daemon" \
+                      "usr/lib/resolvconf/libc.d/mdnsd" "%{_libdir}/resolvconf/libc.d/mdnsd" \
+                      "usr/lib/resolvconf/pdns_recursor" "%{_libdir}/resolvconf/pdns_recursor" \
+                      "usr/lib/resolvconf/named" "%{_libdir}/resolvconf/named" \
+                      "usr/sbin/resolvconf" "%{_sbindir}/resolvconf"
--- builder/utils/1/patch.sh
+++ builder/utils/1/patch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PATCH_SRC=$CACHE/patch
+PATCH_RPM_NAME=Patch
+PATCH_VERSION="2.7.6"
+PATCH_FULLNAME=$PATCH_RPM_NAME-$PATCH_VERSION
 $ROOT/download_and_untar.sh "https://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz" "$PATCH_SRC"
 
 if [ ! -d $PATCH_SRC/build ]
@@ -11,4 +14,17 @@ then
   make -j$(nproc)
 fi
 cd $PATCH_SRC/build
-make DESTDIR=$ISO_SYSROOT install
+#make DESTDIR=$ISO_SYSROOT install
+
+$ROOT/generate_tar.sh make $PATCH_SRC/build/$PATCH_FULLNAME.tar $PATCH_FULLNAME \
+                      usr/bin/patch
+
+$ROOT/generate_rpm.sh "$ISO_RPMS" \
+                      "$PATCH_SRC/build/rpmbuild" \
+                      "$PATCH_RPM_NAME" \
+                      "$PATCH_VERSION" \
+                      "Patch utility" \
+                      "This is GNU patch, which applies diff files to original files." \
+                      "GPL-3.0" \
+                      "$PATCH_SRC/build/$PATCH_FULLNAME.tar" \
+                      "usr/bin/patch" "%{_bindir}/patch"
