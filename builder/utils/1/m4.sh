c4ae1d00f7665089cec327b16f72f60c7c99b34c
VBrawl <konstantosjim@gmail.com>
2025-03-30 05:00:36 +0300

    Added M4 and Make as RPM packages

--- builder/utils/1/m4.sh
+++ builder/utils/1/m4.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 M4_SRC=$CACHE/m4
+M4_RPM_NAME=M4
+M4_VERSION="1.4.19"
+M4_FULLNAME=$M4_RPM_NAME-$M4_VERSION
 $ROOT/download_and_untar.sh "https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.xz" "$M4_SRC"
 
 if [ ! -d $M4_SRC/build ]
@@ -11,4 +14,16 @@ then
   make -j$(nproc)
 fi
 cd $M4_SRC/build
-make DESTDIR=$ISO_SYSROOT install
+#make DESTDIR=$ISO_SYSROOT install
+
+$ROOT/generate_tar.sh make $M4_SRC/build/$M4_FULLNAME.tar $M4_FULLNAME \
+                      usr/bin/m4
+$ROOT/generate_rpm.sh "$ISO_RPMS" \
+                      "$M4_SRC/build/rpmbuild" \
+                      "$M4_RPM_NAME" \
+                      "$M4_VERSION" \
+                      "M4 utility" \
+                      "GNU M4 is an implementation of the traditional Unix macro processor. It is mostly SVR4 compatible although it has some extensions (for example, handling more than 9 positional parameters to macros). GNU M4 also has built-in functions for including files, running shell commands, doing arithmetic, etc." \
+                      "GPL-3" \
+                      "$M4_SRC/build/$M4_FULLNAME.tar" \
+                      "usr/bin/m4" "%{_bindir}/m4"
--- builder/utils/1/make.sh
+++ builder/utils/1/make.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 MAKE_SRC=$CACHE/make
+MAKE_RPM_NAME=Make
+MAKE_VERSION="4.4.1"
+MAKE_FULLNAME=$MAKE_RPM_NAME-$MAKE_VERSION
 $ROOT/download_and_untar.sh "https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz" "$MAKE_SRC"
 
 if [ ! -d $MAKE_SRC/build_dir ]
@@ -11,4 +14,19 @@ then
   make -j$(nproc)
 fi
 cd $MAKE_SRC/build_dir
-make DESTDIR=$ISO_SYSROOT install
+#make DESTDIR=$ISO_SYSROOT install
+
+$ROOT/generate_tar.sh make $MAKE_SRC/build_dir/$MAKE_FULLNAME.tar $MAKE_FULLNAME \
+                      usr/bin/make \
+                      usr/include/gnumake.h
+
+$ROOT/generate_rpm.sh "$ISO_RPMS" \
+                      "$MAKE_SRC/build_dir/rpmbuild" \
+                      "$MAKE_RPM_NAME" \
+                      "$MAKE_VERSION" \
+                      "Make utility" \
+                      "GNU Make is a tool which controls the generation of executables and other non-source files of a program from the program's source files." \
+                      "GPL-3" \
+                      "$MAKE_SRC/build/$MAKE_FULLNAME.tar" \
+                      "usr/bin/make" "%{_bindir}/make" \
+                      "usr/include/gnumake.h" "%{_includedir}/gnumake.h"
