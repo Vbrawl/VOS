d7e37c0e26c62d553ec146b3988a5a584414d499
VBrawl <konstantosjim@gmail.com>
2025-04-09 11:01:11 +0300

    Added InitUp RPM package

--- builder/utils/1/initup.sh
+++ builder/utils/1/initup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 INITUP_SRC=$CACHE/initup
+INITUP_RPM_NAME=InitUp
+INITUP_VERSION="Latest"
+INITUP_FULLNAME=$INITUP_RPM_NAME-$INITUP_VERSION
 
 if [ ! -d $INITUP_SRC ]
 then
@@ -16,4 +19,19 @@ then
 fi
 
 cd $INITUP_SRC/build
-DESTDIR=$ISO_SYSROOT/usr meson install
+$ROOT/generate_tar.sh meson $INITUP_SRC/build/$INITUP_FULLNAME.tar $INITUP_FULLNAME \
+                      bin/initup-gen \
+                      sbin/{initup-init,initup-serviced}
+#DESTDIR=$(pwd)/$INITUP_FULLNAME/usr meson install
+
+$ROOT/generate_rpm.sh "$ISO_RPMS" \
+                      "$INITUP_SRC/build/rpmbuild" \
+                      "$INITUP_RPM_NAME" \
+                      "$INITUP_VERSION" \
+                      "InitUp init system and service manager" \
+                      "InitUp is an init system aiming to be efficient, small and simple." \
+                      "BSD-2" \
+                      "$INITUP_SRC/build/$INITUP_FULLNAME.tar" \
+                      "bin/initup-gen" "%{_bindir}/initup-gen" \
+                      "sbin/initup-init" "%{_sbindir}/initup-init" \
+                      "sbin/initup-serviced" "%{_sbindir}/initup-serviced"
